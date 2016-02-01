171fe6891c76cf3185c7c3a1a7c6fe1bffefbe5a
Stefan Seemayer <stefan@seemayer.de>
2016-02-01 11:35:59 +0100

    Use new max-norm

--- posterior.py
+++ posterior.py
@@ -11,7 +11,7 @@ def parse_args():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("sum_matrix", help="Pair sum score file")
-    parser.add_argument("sum_triplets", help="Triplet sum score file", nargs="?")
+    parser.add_argument("triplets", help="Triplet score file", nargs="?")
     parser.add_argument("out_matrix", help="Output posterior matrix to write")
 
     parser.add_argument("-m", "--model", default="data/triplet_posterior.pickle", help="mixem probability model learned for computing the posterior")
@@ -21,21 +21,47 @@ def parse_args():
     return args
 
 
+def source_triplet(path):
+    try:
+        mat = pd.read_table(path, sep="\t", comment="#", header=None)
+    except:
+        return None
+
+    if len(mat.columns) not in (4, 7):
+        return None
+
+    elif len(mat.columns) == 4:
+        mat.columns = ['i', 'j', 'k', 'confidence']
+
+    elif len(mat.columns) == 7:
+        mat.columns = ['i', 'j', 'k', 'a', 'b', 'c', 'confidence']
+
+
+    return mat
+
+def load_triplets(tripletfile):
+    triplets = source_triplet(tripletfile)
+
+    triplet_scores = collections.defaultdict(float)
+    for _, trp in triplets.iterrows():
+        i, j, k = (int(t) for t in trp[['i', 'j', 'k']])
+
+        triplet_scores[i, j] = max(triplet_scores[i, j], trp.confidence ** 2)
+        triplet_scores[j, k] = max(triplet_scores[j, k], trp.confidence ** 2)
+        triplet_scores[i, k] = max(triplet_scores[i, k], trp.confidence ** 2)
+
+    return triplet_scores
+
+
 def main():
     args = parse_args()
 
     sum_matrix = np.loadtxt(args.sum_matrix)
 
-    triplets = collections.defaultdict(list)
+    triplets = None
+    if args.triplets:
+        triplets = load_triplets(args.triplets)
 
-    if args.sum_triplets:
-        sum_triplets = pd.read_table(args.sum_triplets, header=None, comment="#")
-        assert(sum_triplets.shape[1] == 4)
-        sum_triplets.columns = ['i', 'j', 'k', 'score']
-        for _, trp in sum_triplets.iterrows():
-            triplets[(trp.i, trp.j)].append(trp.score)
-            triplets[(trp.j, trp.k)].append(trp.score)
-            triplets[(trp.i, trp.k)].append(trp.score)
 
     with open(args.model, "rb") as f_model:
         model = pickle.load(f_model)
@@ -59,10 +85,10 @@ def main():
                 np.log(mixem.probability(sum_matrix[i, j], *pair_model[1]))
             )
 
-            for trp in triplets[i, j]:
+            if triplets is not None and (i, j) in triplets:
                 log_bf += (
-                    np.log(mixem.probability(trp, *triplet_model[0])) -
-                    np.log(mixem.probability(trp, *triplet_model[1]))
+                    np.log(mixem.probability(triplets[(i, j)], *triplet_model[0])) -
+                    np.log(mixem.probability(triplets[(i, j)], *triplet_model[1]))
                 )
 
             posterior_matrix[i, j] = log_bf
